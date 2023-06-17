e7fd26162119d342f626ba24b4261623c0d29326
Envinius <envinius@gmail.com>
2023-06-17 18:23:25 -0400

    Update MainController.lua
    Hooray

--- ReplicatedStorage/MainController.lua
+++ ReplicatedStorage/MainController.lua
@@ -377,8 +377,13 @@ function controller.OnQuery()
 			--[[Setting the Answers Up]]--
 			for i,v in(game.ReplicatedStorage.MainController.Questions[question]:GetChildren()) do 
 				if not v:GetAttribute("Assigned") then 
-					v:SetAttribute("Assigned", math.random(i))
+					v:SetAttribute("Assigned", i)
 				end
+			end
+			
+			task.wait(.5)
+			
+			for i,v in(game.ReplicatedStorage.MainController.Questions[question]:GetChildren()) do 
 				if v:GetAttribute("Assigned") then 
 					if v:GetAttribute("Assigned") == 1 then 
 						One.Text = v.Name 
@@ -391,7 +396,6 @@ function controller.OnQuery()
 					end
 				end
 			end
-			
 			
 			task.wait(.7)
 			TweenBase(One, 1, "TextTransparency", 0)
@@ -406,17 +410,48 @@ function controller.OnQuery()
 			TweenBase(Four, 1, "TextTransparency", 0)
 			TweenBase(Four, 1, "BackgroundTransparency", Four:GetAttribute("BackgroundTransparency"))
 			
+			local Selects = false 
+			local CanSelect = true 
 			for _,v in(Frame.Answers:GetChildren()) do 
 				v.Activated:Connect(function() 
-					for _,v in(Frame.Answers:GetChildren()) do 
-						TweenBase(v, 1, "TextColor3", Color3.fromRGB(255, 56, 56))
+					if CanSelect then 
+						for _,v in(Frame.Answers:GetChildren()) do 
+							TweenBase(v, 1, "TextColor3", Color3.fromRGB(255, 56, 56))
+						end
+						TweenBase(v, 1, "TextColor3", Color3.fromRGB(144, 255, 148))
+						TweenBase(Submit, 1, "TextTransparency", 0)
+						TweenBase(Submit, 1, "BackgroundTransparency", Submit:GetAttribute("BackgroundTransparency"))
+						Selected.Value = v.Text 
+						Selects = true 
+						
 					end
-					TweenBase(v, 1, "TextColor3", Color3.fromRGB(144, 255, 148))
-					TweenBase(Submit, 1, "TextTransparency", 0)
-					TweenBase(Submit, 1, "BackgroundTransparency", Submit:GetAttribute("BackgroundTransparency"))
 				end)
 			end
 			
+			Submit.Activated:Connect(function()
+				if Selects then 
+					CanSelect = false 
+					Selects = false 
+					
+					TweenBase(Submit, 1, "TextTransparency", 1)
+					TweenBase(Submit, 1, "BackgroundTransparency", 1)
+
+					Title.Text = "Answer Locked in: " 
+					TweenBase(Title, 1, "TextColor3", Color3.fromRGB(181, 218, 177))
+					
+					for _,newv in(Frame.Answers:GetChildren()) do
+						if newv.Text ~= Selected.Value then 
+							TweenBase(newv, 1, "TextTransparency", 1)
+						end 
+					end
+					
+					-- 
+					for _,v in(Frame.Answers:GetChildren()) do 
+						v:TweenPosition(Frame.Answers:GetAttribute("EndPositions"), Enum.EasingDirection.Out, Enum.EasingStyle.Linear, 1.5)
+					end
+					
+				end
+			end)
 			
 		end
 	end)
